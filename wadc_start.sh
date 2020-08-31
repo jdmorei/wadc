1b59a84826127cd3ab34414593f5a56dee668368
jdmorei <admin@wadc.es>
2020-08-31 23:47:12 +0200

    change webssh policy

--- wadc_start.sh
+++ wadc_start.sh
@@ -2,7 +2,7 @@
 
 kill $(ps -aux | grep wssh | awk '{print $2}' )
 
-wssh  --certfile='certs/server.crt' --keyfile='certs/server.key' --address='127.0.0.1' --port=54321 --sslport=65432 --log-file-prefix='public/log/wssh.log' --xheaders=False --policy=reject&
+wssh  --certfile='certs/server.crt' --keyfile='certs/server.key' --address='127.0.0.1' --port=54321 --sslport=65432 --log-file-prefix='public/log/wssh.log' --xheaders=False &
 
 rails server
 
